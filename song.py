6faf138d3bacb437e3407c841f6787b35d9c9a3e
Kendi <muriungikendileila@gmail.com>
2024-02-09 16:41:27 +0300

    Add files via upload

---
+++ song.py
@@ -0,0 +1,63 @@
+
+
+def swallow_fly():
+    print("There was an old woman who swallowed a fly.")
+    print("I don't know why she swallowed that fly,")
+    print("Perhaps she'll die.")
+    print()
+
+def swallow_spider():
+    print("There was an old woman who swallowed a spider,")
+    print("That wriggled and iggled and jiggled inside her.")
+    print("She swallowed the spider to catch the fly,")
+    print("I don't know why she swallowed that fly,")
+    swallow_fly()
+
+def swallow_bird():
+   print("There was an old woman who swallowed a bird,")
+   print("How absurd to swallow a bird.")
+   print("She swallowed the bird to catch the spider,")
+   print()
+   swallow_spider()
+
+def swallow_cat():
+    print("There was an old woman who swallowed a cat,")
+    print("Imagine that to swallow a cat.")
+    print("She swallowed the cat to catch the bird,")
+    print()
+    swallow_bird()
+
+def swallow_dog():
+    print("There was an old woman who swallowed a dog,")
+    print("What a hog to swallow a dog.")
+    print("She swallowed the dog to catch the cat,")
+    print()
+    swallow_cat()
+
+def swallow_snake():
+    print("There was an old woman who swallowed a snake,")
+    print("So horryfying to swallow a snake.")
+    print("She swallowed the snake to catch the dog")
+    print()
+    swallow_dog()
+
+
+def swallow_horse():
+    print("There was an old woman who swallowed a horse,")
+    print("She died of course.")
+
+
+def song():
+    swallow_fly()
+    swallow_spider()
+    swallow_bird()
+    swallow_cat()
+    swallow_dog()
+    swallow_snake()
+    swallow_horse()
+
+
+song()
+
+
+    
